1146bc55be13e80dc649cb2565656374808d1e19
mcintja <mcintja@cat.com>
2019-02-13 14:23:47 -0600

    Added Create Task button to views/projects/display, create_task method to the Task_model, and create view to Tasks.php

--- application/controllers/Tasks.php
+++ application/controllers/Tasks.php
@@ -14,7 +14,32 @@ class Tasks extends CI_Controller
     }
 
 
-    
+
+    public function create($project_id)
+    {
+        $this->form_validation->set_rules('task_name', 'Task Name', 'trim|required');
+        $this->form_validation->set_rules('due_date', 'Task Due Date', 'trim|required');
+        $this->form_validation->set_rules('task_body', 'Task Description', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+            $data['main_view'] = 'tasks/create_task';
+            $this->load->view('layouts/main', $data);
+        } else {
+            $data = array(
+                'task_name' => $this->input->post('task_name'),
+                'due_date' => $this->input->post('due_date'),
+                'task_body' => $this->input->post('task_body'),
+                'project_id' => $project_id
+                );
+
+            if ($this->task_model->create_task($data)) {
+                $this->session->set_flashdata('task_created', 'Your Task has been created');
+
+                redirect("projects/display/$project_id");
+            }
+        }
+    }
+
     
     // your new methods go here
     
--- application/models/Task_model.php
+++ application/models/Task_model.php
@@ -2,6 +2,12 @@
 
 class Task_model extends CI_Model {
 
+ 	public function create_task($data) {
+		$insert_query = $this->db->insert('tasks', $data);
+
+		return $insert_query;
+	}
+ 
 	public function get_task($task_id) {
 		$this->db->where('id', $task_id);
 		$query = $this->db->get('tasks');
